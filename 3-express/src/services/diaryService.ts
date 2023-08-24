af2f7ce8e3539abd3cace8ba16fa0d3e0b45dab6
Gavin Garza <gaving@live.com>
2023-08-24 22:53:31 +0900

    added type safety to diary post request

--- 3-express/src/services/diaryService.ts
+++ 3-express/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaryData from "../../data/entries"
-import { DiaryEntry, NonSensitiveDiaryEntry } from "../types"
+import { DiaryEntry, NonSensitiveDiaryEntry, NewDiaryEntry } from "../types"
 
 const diaries: DiaryEntry[] = diaryData
 
@@ -16,12 +16,23 @@ const getNonsensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }))
 }
 
-const addDiary = () => {
-  return null
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  }
+  diaries.push(newDiaryEntry)
+  return newDiaryEntry
+}
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id)
+  return entry
 }
 
 export default {
   getEntries,
   getNonsensitiveEntries,
   addDiary,
+  findById,
 }
