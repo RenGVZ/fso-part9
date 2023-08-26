420477e8c5871fb903be0e7f69385b5b81ba08ef
Gavin Garza <gaving@live.com>
2023-08-26 14:16:38 +0900

    react ts app setup

--- 3-express/backend/build/src/index.js
+++ 3-express/backend/build/src/index.js
@@ -4,13 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.get("/ping", (_req, res) => {
-    console.log("someone pinged here");
-    res.send("pong!");
-});
 const PORT = 3000;
+app.use("/api/diaries", diaries_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
